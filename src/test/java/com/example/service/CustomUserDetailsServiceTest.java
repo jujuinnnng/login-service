8c98ac72a1c3e6749dbac3f549a2fb2d28cc315f
Seungju Seo <seosj3104@naver.com>
2023-12-03 20:24:28 +0900

    네임변경

--- src/test/java/com/example/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/example/service/CustomUserDetailsServiceTest.java
@@ -1,13 +1,12 @@
 package com.example.service;
 
-import com.example.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @SpringBootTest
-class UserServiceTest {
+class CustomUserDetailsServiceTest {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
